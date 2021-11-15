0c46206193f7cf10f0a69f0702c7df8402b4eb44
aleksey-ilin <ilinalekseys@gmail.com>
2021-11-15 12:41:28 +0300

    up cpress

--- cypress/integration/autoscroll.spec.js
+++ cypress/integration/autoscroll.spec.js
@@ -8,7 +8,9 @@ describe('autoscroll', () => {
   it('should not work', () => {
     cy.findByText('open side panel').click();
     // fix https://github.com/cypress-io/cypress/pull/18441
+    // work in cypress since 9.0.0
     cy.findByText('close side panel').click();
+    //hack for cypress before 9.0.0
     // cy.findByText('close side panel').click({ scrollBehavior:false });
   })
 })
